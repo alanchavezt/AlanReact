fa7f37e554c9a53fafccf6d6104aafd34002220b
alanchavezt1 <a.chavez.1@elsevier.com>
2021-06-23 15:02:54 -0600

    creating components for InputText

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Home.css';
+import {Button} from "react-bootstrap";
 
 function Home () {
 
     return (
         <div>
-            Welcome to the Home Page!
+            <div>
+                <h1>Welcome To My Personal Portfolio</h1>
+                <p>The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.</p>
+                <div className="form-group">
+                    <button type="button" className="btn btn-primary">Learn more</button>
+                </div>
+            </div>
+
+            <div>
+                <h1>Projects</h1>
+            </div>
+            <div>
+                <h1>Technologies</h1>
+                <p>I've worked with a range a technologies in the web development world. From Back-end To Design</p>
+            </div>
+            <div>
+                <h1>About me</h1>
+                <p>The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.</p>
+            </div>
+            <div>
+                <h1>Personal Achievements</h1>
+            </div>
         </div>
     );
 }
--- src/components/users/UserFormEntry.jsx
+++ src/components/users/UserFormEntry.jsx
@@ -1,67 +1,103 @@
 import React from 'react';
 import './Users.css';
 import Loading from "../common/Loading";
+import InputText from "../common/form/InputText";
+import {PropTypes} from "prop-types";
+import CheckBox from "../common/form/CheckBox";
 
 export default class UserFormEntry extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            user: this.props.user
-        };
     }
 
     onFormChange = (e) => {
-        let user = this.state.user;
+        let user = this.props.user;
         user[e.target.name] = e.target.value;
-        this.setState({user}, () => {
-            if (this.props.onChange) {
-                this.props.onChange(this.state.user);
-            }
-        });
+
+        if (this.props.onChange) {
+            this.props.onChange(this.props.user);
+        }
     }
 
     render () {
-        if (!this.state.user) {
+        if (!this.props.user) {
             return <Loading />
         }
         return (
             <form onChange={this.onFormChange}>
-                <div className="form-group">
-                    <label className="float-left" htmlFor="firstName">First Name</label>
-                    <input type="text" className="form-control" id="firstName" defaultValue={this.state.user.name}/>
-                </div>
+                <InputText
+                    label="First Name"
+                    id="firstName"
+                    name="firstName"
+                    value={this.props.user.firstName || this.props.user.name}
+                    // onChange={this.onFormChange}
+                    required={true}
+                />
+                <InputText
+                    label="Last Name"
+                    id="lastName"
+                    name="lastName"
+                    value={this.props.user.lastName || ""}
+                    // onChange={this.onFormChange}
+                    required={true}
+                />
+                <InputText
+                    label="Username"
+                    id="username"
+                    name="username"
+                    value={this.props.user.username}
+                    // onChange={this.onFormChange}
+                    required={true}
+                />
+                <InputText
+                    label="Email address"
+                    id="email"
+                    name="email"
+                    value={this.props.user.email}
+                    // onChange={this.onFormChange}
+                    required={true}
+                />
+
                 {/*<div className="form-group">*/}
-                {/*    <label className="float-left" htmlFor="lastName">Last Name</label>*/}
-                {/*    <input type="text" className="form-control" id="lastName"/>*/}
+                {/*    <label className="float-left" htmlFor="email">Email address</label>*/}
+                {/*    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.props.user.email}/>*/}
+                {/*    <small id="emailHelp" className="form-text text-muted float-right">We'll never share your email with*/}
+                {/*        anyone else.</small>*/}
                 {/*</div>*/}
-                <div className="form-group">
-                    <label className="float-left" htmlFor="username">Username</label>
-                    <input type="text" className="form-control" id="username" defaultValue={this.state.user.username}/>
-                </div>
-                <div className="form-group">
-                    <label className="float-left" htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" defaultValue={this.state.user.email}/>
-                    <small id="emailHelp" className="form-text text-muted float-right">We'll never share your email with
-                        anyone else.</small>
-                </div>
                 {/*<div className="form-group">*/}
                 {/*    <label className="float-left" htmlFor="password">Password</label>*/}
                 {/*    <input type="password" className="form-control" id="password"/>*/}
                 {/*</div>*/}
-                <div className="form-group">
-                    <label className="float-left" htmlFor="phone">Phone</label>
-                    <input type="text" className="form-control" id="phone" defaultValue={this.state.user.phone}/>
-                </div>
-                <div className="form-group">
-                    <label className="float-left" htmlFor="website">Website</label>
-                    <input type="text" className="form-control" id="website" defaultValue={this.state.user.website}/>
-                </div>
+
+                <InputText
+                    label="Phone"
+                    id="phone"
+                    name="phone"
+                    value={this.props.user.phone}
+                    onChange={this.onFormChange}
+                    required={true}
+                />
+                <InputText
+                    label="Website"
+                    id="website"
+                    name="website"
+                    value={this.props.user.website}
+                    onChange={this.onFormChange}
+                    required={true}
+                />
+                <CheckBox
+                    label="Check me out"
+                    id="checkMeOut"
+                    name="checkMeOut"
+                    checked={true}
+                    onChange={this.onFormChange}
+                />
             </form>
         );
     }
 }
 
-// UserFormEntry.propTypes = {
-//     user: PropTypes.object
-// }
+UserFormEntry.propTypes = {
+    user: PropTypes.object
+}

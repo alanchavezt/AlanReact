33f7c994aa767339b681acd492c4f77f3c214da9
Alan Chavez <alanchavez1@gmail.com>
2021-09-23 17:23:20 -0600

    styling the header and nav bar

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -8,50 +8,58 @@ const Navbar = (props) => {
 
     return (
         <section className="navbar">
-            <Link className="navbar-item" to='/home'>
-                <span>Home</span>
-            </Link>
-            <Link className="navbar-item" to='/about'>
-                <span>About</span>
-            </Link>
-            <Link className="navbar-item" to='/portfolio'>
-                <span>Portfolio</span>
-            </Link>
-            <Link className="navbar-item" to='/shop'>
-                <span>Shop</span>
-            </Link>
-            <Link className="navbar-item" to='/blog'>
-                <span>Blog</span>
-            </Link>
-            <Link className="navbar-item" to='/contact'>
-                <span>Contact</span>
-            </Link>
+            <div className="navbar-flex-item-container">
+                <Link className="navbar-item" to='/home'>
+                    <span>Home</span>
+                </Link>
+                <Link className="navbar-item" to='/about'>
+                    <span>About</span>
+                </Link>
+                <Link className="navbar-item" to='/portfolio'>
+                    <span>Portfolio</span>
+                </Link>
+                <Link className="navbar-item" to='/shop'>
+                    <span>Shop</span>
+                </Link>
+                <Link className="navbar-item" to='/blog'>
+                    <span>Blog</span>
+                </Link>
+                <Link className="navbar-item" to='/contact'>
+                    <span>Contact</span>
+                </Link>
+            </div>
 
-            {getToken() ?
-                <React.Fragment>
-                    <Link className="navbar-item" to='/dashboard'>
-                        <span>Dashboard</span>
-                    </Link>
-                    <Link className="navbar-item" to='/users'>
-                        <span>Users</span>
-                    </Link>
-                </React.Fragment>
-                : null
-            }
+            <div className="navbar-flex-item-container">
+                {getToken() ?
+                    <React.Fragment>
+                        <Link className="navbar-item" to='/dashboard'>
+                            <span>Dashboard</span>
+                        </Link>
+                        <Link className="navbar-item" to='/users'>
+                            <span>Users</span>
+                        </Link>
+                    </React.Fragment>
+                    : null
+                }
 
-            {!getToken() ?
-                <React.Fragment>
-                    <Link className="navbar-item" to='/signin'>
-                        <span>Sign In</span>
-                    </Link>
-                    <Link className="navbar-item" to='/signup'>
-                        <span>Sign up</span>
-                    </Link>
-                </React.Fragment>
-                : null
-            }
+                {!getToken() ?
+                    <React.Fragment>
+                        <Link className="navbar-item" to='/signin'>
+                            <span>Sign In</span>
+                        </Link>
+                        <Link className="navbar-item" to='/signup'>
+                            <span>Sign up</span>
+                        </Link>
+                    </React.Fragment>
+                    : null
+                }
 
-            {getToken() ? <Avatar/> : null}
+                {getToken() ? <div className="navbar-item"><Avatar/></div> : null}
+            </div>
+
+            {/*<div className="navbar-flex-item-container">*/}
+            {/*    {getToken() ? <div className="navbar-item"><Avatar/></div> : null}*/}
+            {/*</div>*/}
         </section>
     );
 };

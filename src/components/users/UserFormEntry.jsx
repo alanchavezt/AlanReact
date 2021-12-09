36093d2716b5ce647d736f7cfda55e8f83a82365
Alan Chavez <alanchavez1@gmail.com>
2021-12-09 15:54:02 -0700

    converting UserEditEntry and UserFormEntry class component to functional component

--- src/components/users/UserFormEntry.jsx
+++ src/components/users/UserFormEntry.jsx
@@ -1,110 +1,107 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Users.css';
 import Loading from "../common/Loading";
 import InputText from "../common/form/InputText";
 import {PropTypes} from "prop-types";
 import CheckBox from "../common/form/CheckBox";
 
-export default class UserFormEntry extends React.Component {
+const UserFormEntry = (props) => {
 
-    constructor(props) {
-        super(props);
-    }
-
-    onFormChange = (e) => {
-        let user = this.props.user;
+    const onFormChange = (e) => {
+        let user = {...props.user};
         user[e.target.name] = e.target.value;
 
-        if (this.props.onChange) {
-            this.props.onChange(this.props.user);
+        if (props.onChange) {
+            props.onChange(user);
         }
     }
 
-    render () {
-        if (!this.props.user) {
-            return <Loading />
-        }
-        return (
-            <form onChange={this.onFormChange}>
-                <InputText
-                    // style={{display: this.props.user.userId ? "inline" : "none"}}
-                    label="User ID"
-                    id="userId"
-                    name="userId"
-                    value={this.props.user.userId}
-                    readOnly={true}
-                />
-                <InputText
-                    label="Username"
-                    id="username"
-                    name="username"
-                    value={this.props.user.username}
-                    required={true}
-                />
-                <InputText
-                    label="Password"
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={this.props.user.password}
-                    required={true}
-                />
-                <InputText
-                    label="First Name"
-                    id="firstName"
-                    name="firstName"
-                    value={this.props.user.firstName}
-                    required={true}
-                />
-                <InputText
-                    label="Middle Name"
-                    id="middleName"
-                    name="middleName"
-                    value={this.props.user.middleName}
-                    required={true}
-                />
-                <InputText
-                    label="Last Name"
-                    id="lastName"
-                    name="lastName"
-                    value={this.props.user.lastName}
-                    required={true}
-                />
-                <InputText
-                    label="Email address"
-                    id="email"
-                    name="email"
-                    value={this.props.user.email}
-                    required={true}
-                />
-                {/*<InputText*/}
-                {/*    label="Phone"*/}
-                {/*    id="phone"*/}
-                {/*    name="phone"*/}
-                {/*    value={this.props.user.phone}*/}
-                {/*    onChange={this.onFormChange}*/}
-                {/*    required={true}*/}
-                {/*/>*/}
-                {/*<InputText*/}
-                {/*    label="Website"*/}
-                {/*    id="website"*/}
-                {/*    name="website"*/}
-                {/*    value={this.props.user.website}*/}
-                {/*    onChange={this.onFormChange}*/}
-                {/*    required={true}*/}
-                {/*/>*/}
-                {/*<CheckBox*/}
-                {/*    label="Check me out"*/}
-                {/*    id="checkMeOut"*/}
-                {/*    name="checkMeOut"*/}
-                {/*    checked={true}*/}
-                {/*    onChange={this.onFormChange}*/}
-                {/*/>*/}
-            </form>
-        );
+    if (!props.user) {
+        return <Loading />
     }
+
+    return (
+        <form onChange={onFormChange}>
+            <InputText
+                // style={{display: props.user.userId ? "inline" : "none"}}
+                label="User ID"
+                id="userId"
+                name="userId"
+                value={props.user.userId}
+                readOnly={true}
+            />
+            <InputText
+                label="Username"
+                id="username"
+                name="username"
+                value={props.user.username}
+                required={true}
+            />
+            <InputText
+                label="Password"
+                type="password"
+                id="password"
+                name="password"
+                value={props.user.password}
+                required={true}
+            />
+            <InputText
+                label="First Name"
+                id="firstName"
+                name="firstName"
+                value={props.user.firstName}
+                required={true}
+            />
+            <InputText
+                label="Middle Name"
+                id="middleName"
+                name="middleName"
+                value={props.user.middleName}
+                required={true}
+            />
+            <InputText
+                label="Last Name"
+                id="lastName"
+                name="lastName"
+                value={props.user.lastName}
+                required={true}
+            />
+            <InputText
+                label="Email address"
+                id="email"
+                name="email"
+                value={props.user.email}
+                required={true}
+            />
+            {/*<InputText*/}
+            {/*    label="Phone"*/}
+            {/*    id="phone"*/}
+            {/*    name="phone"*/}
+            {/*    value={props.user.phone}*/}
+            {/*    onChange={onFormChange}*/}
+            {/*    required={true}*/}
+            {/*/>*/}
+            {/*<InputText*/}
+            {/*    label="Website"*/}
+            {/*    id="website"*/}
+            {/*    name="website"*/}
+            {/*    value={props.user.website}*/}
+            {/*    onChange={onFormChange}*/}
+            {/*    required={true}*/}
+            {/*/>*/}
+            {/*<CheckBox*/}
+            {/*    label="Check me out"*/}
+            {/*    id="checkMeOut"*/}
+            {/*    name="checkMeOut"*/}
+            {/*    checked={true}*/}
+            {/*    onChange={onFormChange}*/}
+            {/*/>*/}
+        </form>
+    );
 }
 
 UserFormEntry.propTypes = {
     user: PropTypes.object
 }
+
+export default UserFormEntry;

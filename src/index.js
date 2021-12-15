75540362f16cf1ee2664a1b07c029b2f4f766227
Alan Chavez <alanchavez1@gmail.com>
2021-12-14 22:52:22 -0700

    adding popper, importating bootstrap bundle and fixing syntaxis for the dropdown

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {getToken} from "./utils/Common";
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/js/bootstrap.js';
 
 

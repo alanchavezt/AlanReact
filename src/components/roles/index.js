25531004f87be67ef5914e4b8d9c2f030ff151ce
Alan Chavez <alanchavez1@gmail.com>
2022-02-16 21:40:26 -0700

    Adding Role View Component and registering on the App.js

--- src/components/roles/index.js
+++ src/components/roles/index.js
@@ -1,5 +1,6 @@
 import RoleListEntry from "./RoleListEntry";
 import RoleCreateEntry from "./RoleCreateEntry";
 import RoleEditEntry from "./RoleEditEntry";
+import RoleViewEntry from "./RoleViewEntry";
 
-export { RoleListEntry, RoleCreateEntry, RoleEditEntry };
+export { RoleListEntry, RoleCreateEntry, RoleEditEntry, RoleViewEntry };
--- src/components/roles/routes.js
+++ src/components/roles/routes.js
@@ -4,11 +4,12 @@ import {lazy} from "react";
 const RoleListEntry = lazy(() => import('./RoleListEntry'));
 const RoleCreateEntry = lazy(() => import('./RoleCreateEntry'));
 const RoleEditEntry = lazy(() => import('./RoleEditEntry'));
+const RoleViewEntry = lazy(() => import('./RoleViewEntry'));
 
 const routes = [
     {name: "Roles", Component: RoleListEntry, path: "/roles"},
     {name: "Create", Component: RoleCreateEntry, path: "/roles/create"},
-    // {name: "View", Component: RoleViewEntry, path: "/roles/:id"},
+    {name: "View", Component: RoleViewEntry, path: "/roles/:id"},
     {name: "Edit", Component: RoleEditEntry, path: "/roles/:id/edit"}
 ];
 

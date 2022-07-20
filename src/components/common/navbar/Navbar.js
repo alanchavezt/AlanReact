fbadd02cbe2a9380f552e539bfdddae188ea7c14
Alan Chavez <alanchavez1@gmail.com>
2022-07-20 16:48:53 -0600

    adding more web responsive styling

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
 
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggleClick = () => {
+        setToggle(toggle => !toggle);
+    }
+
+    const active = toggle ? "active" : "";
+
     return (
         <div className="navbar">
-            <Link className="navbar-item" to='/home'>
-                <span>Home</span>
-            </Link>
-            <Link className="navbar-item" to='/about'>
-                <span>About</span>
-            </Link>
-            <Link className="navbar-item" to='/portfolio'>
-                <span>Portfolio</span>
-            </Link>
-            <Link className="navbar-item" to='/shop'>
-                <span>Shop</span>
-            </Link>
-            <Link className="navbar-item" to='/blog'>
-                <span>Blog</span>
-            </Link>
-            <Link className="navbar-item" to='/contact'>
-                <span>Contact</span>
-            </Link>
+            <a href="#" className="toggle-button" onClick={handleToggleClick}>
+                <span className="bar"/>
+                <span className="bar"/>
+                <span className="bar"/>
+            </a>
+            <div className={`navbar-links ${active}`}>
+                <ul>
+                    <li>
+                        <Link to='/home'>
+                            <span>Home</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>
+                            <span>About</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/portfolio'>
+                            <span>Portfolio</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/shop'>
+                            <span>Shop</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/blog'>
+                            <span>Blog</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>
+                            <span>Contact</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };

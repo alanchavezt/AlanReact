3bfee20c97af83f3ae47b79e021f7cae24979925
Alan Chavez <alanchavez1@gmail.com>
2022-07-27 13:50:29 -0600

    makind some changes to the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     context: __dirname,
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: '[name].js',
         publicPath: "/"
     },
